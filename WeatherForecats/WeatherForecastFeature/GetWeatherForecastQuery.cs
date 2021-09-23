3d95ddafb1ce21501abefad39ce203ae45755097
Bogdan Polak <bogdanpol@gmail.com>
2021-09-23 15:38:11 +0200

    Refactoring

--- WeatherForecats/WeatherForecastFeature/GetWeatherForecastQuery.cs
+++ WeatherForecats/WeatherForecastFeature/GetWeatherForecastQuery.cs
@@ -8,10 +8,15 @@ namespace WeatherForecats.WeatherForecastFeature
 {
     public class GetWeatherForecastQuery : IRequest<IEnumerable<WeatherForecast>>
     {
-        public int Day;
-        public GetWeatherForecastQuery(int day)
+        public int Day { get; }
+        
+        public GetWeatherForecastQuery(int? day)
         {
-            Day = day;
+            if (day is null)
+                throw new ArgumentNullException(nameof(day));
+            if (day is < 1 or > 14)
+                throw new ArgumentException("Expected value between 1..14",nameof(day));
+            Day = day.Value;
         }
     }
 }
