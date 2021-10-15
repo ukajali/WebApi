884855e4c7eb4f84c8c2416b2568b3c2c3568f6f
Bogdan Polak <bogdanpol@gmail.com>
2021-10-15 13:37:31 +0200

    Added unit test project

---
+++ UnitTests/GetWeatherForecastQueryTests.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Data;
+using Moq;
+using WeatherForecast.Repositorie;
+using WeatherForecast.WeatherForecastFeature;
+using Xunit;
+
+namespace UnitTests
+{
+    public class GetWeatherForecastQueryTests
+    {
+        private readonly GetWeatherForecastQueryHandler _sut;
+        private readonly Mock<ITemperatureRepository> _temperatureRepositoryMock;
+
+        public GetWeatherForecastQueryTests()
+        {
+            _temperatureRepositoryMock = new Mock<ITemperatureRepository>();
+            _sut = new GetWeatherForecastQueryHandler(_temperatureRepositoryMock.Object);
+        }
+        [Fact]
+        public void Test1()
+        {
+            _sut.Handle(new GetWeatherForecastQuery(5,"germany/bonn"))
+        }
+    }
+}
