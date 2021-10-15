b4e4a4947d92c809a57628f62f2cce89b0d91530
Bogdan Polak <bogdanpol@gmail.com>
2021-10-15 14:47:07 +0200

    Updated unit test

--- UnitTests/GetWeatherForecastQueryTests.cs
+++ UnitTests/GetWeatherForecastQueryTests.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using Moq;
+using WeatherForecast.Model;
 using WeatherForecast.Repositories;
 using WeatherForecast.WeatherForecastFeature;
 using Xunit;
@@ -16,10 +20,22 @@ namespace UnitTests
             _temperatureRepositoryMock = new Mock<ITemperatureRepository>();
             _sut = new GetWeatherForecastQueryHandler(_temperatureRepositoryMock.Object);
         }
+        
         [Fact]
-        public void Test1()
+        public async Task WhenDays5_And_LocationBonn()
         {
-            _sut.Handle(new GetWeatherForecastQuery(5, "germany/bonn"), CancellationToken.None);
+            // arrange
+            const string location = "germany/bonn";
+            const int days = 5;
+            _temperatureRepositoryMock
+                .Setup(x => x.Get(location))
+                .Returns(new TemperatureRange(0, 10));
+            
+            // act
+            var forecasts = await _sut.Handle(new GetWeatherForecastQuery(days, location), CancellationToken.None);
+
+            // assert
+            Assert.Equal(DateTime.Now.AddDays(1), forecasts.ToArray()[0].Date);
         }
     }
 }
