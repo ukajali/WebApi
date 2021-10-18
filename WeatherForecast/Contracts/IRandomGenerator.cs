efcc9b5334b01a8543e2311c892567a873ec4ec9
Bogdan Polak <bogdanpol@gmail.com>
2021-10-18 10:01:12 +0200

    Added contracts

---
+++ WeatherForecast/Contracts/IRandomGenerator.cs
@@ -0,0 +1,7 @@
+﻿namespace WeatherForecast.Contracts
+{
+    public interface IRandomGenerator
+    {
+        int GetRange(int low, int high);
+    }
+}
--- WeatherForecast/WeatherForecastFeature/GetWeatherForecastQueryHandler.cs
+++ WeatherForecast/WeatherForecastFeature/GetWeatherForecastQueryHandler.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using WeatherForecast.Contracts;
 using WeatherForecast.Model;
 using WeatherForecast.Repositories;
 
@@ -16,24 +17,30 @@ namespace WeatherForecast.WeatherForecastFeature
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
         private readonly ITemperatureRepository _temperatureRepository;
+        private readonly INowProvider _nowProvider;
+        private readonly IRandomGenerator _randomGenerator;
 
-        public GetWeatherForecastQueryHandler(ITemperatureRepository temperatureRepository)
+        public GetWeatherForecastQueryHandler(
+            ITemperatureRepository temperatureRepository,
+            INowProvider nowProvider,
+            IRandomGenerator randomGenerator)
         {
             _temperatureRepository = temperatureRepository;
+            _nowProvider = nowProvider;
+            _randomGenerator = randomGenerator;
         }
         public Task<IEnumerable<Model.WeatherForecast>> Handle(GetWeatherForecastQuery request, CancellationToken cancellationToken)
         {
-            var rng = new Random();
             var temperatureRange = _temperatureRepository.Get(request.Location);
             Validate(temperatureRange);
 
-            var startDate = TimeProvider.Current.UtcNow;
+            var startDate = _nowProvider.Now();
 
             var weatherForecast =  Enumerable.Range(1, request.Days).Select(index => new Model.WeatherForecast
             {
                 Date = startDate.AddDays(index),
-                TemperatureC = rng.Next(temperatureRange.Low, temperatureRange.High),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                TemperatureC = _randomGenerator.GetRange(temperatureRange.Low, temperatureRange.High),
+                Summary = Summaries[_randomGenerator.GetRange(0, Summaries.Length)]
             }).AsEnumerable();
 
             return Task.FromResult(weatherForecast);
