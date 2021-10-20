f08fffc898b99e0967c4f876d32eed7c084beaf4
Bogdan Polak <bogdanpol@gmail.com>
2021-10-20 17:03:22 +0200

    Added FakeRandomGenerator and cleaned

---
+++ UnitTests/Fakes/FakeRandomGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using WeatherForecast.Contracts;
+
+namespace UnitTests.Fakes
+{
+    public class FakeRandomGenerator : IRandomGenerator
+    {
+        private readonly Random _random;
+
+        public FakeRandomGenerator(int seed)
+        {
+            _random = new Random(seed);
+        }
+
+        public int GetRange(int low, int high) => _random.Next(low, high);
+    }
+}
--- UnitTests/GetWeatherForecastQueryTests.cs
+++ UnitTests/GetWeatherForecastQueryTests.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Moq;
-using UnitTests.FakesImplementation;
+using UnitTests.Fakes;
 using WeatherForecast.Contracts;
 using WeatherForecast.Model;
 using WeatherForecast.Repositories;
@@ -17,40 +17,43 @@ namespace UnitTests
         private readonly GetWeatherForecastQueryHandler _sut;
         private readonly Mock<ITemperatureRepository> _temperatureRepositoryMock;
         private readonly Mock<INowProvider> _fakeNowProvider;
-        private readonly Mock<IRandomGenerator> _randomGenerator;
+
+        private const string AnyLocation = "germany/bonn";
 
         public GetWeatherForecastQueryTests()
         {
             _temperatureRepositoryMock = new Mock<ITemperatureRepository>();
-            _randomGenerator = new Mock<IRandomGenerator>();
+            _temperatureRepositoryMock
+                .Setup(x => x.Get(AnyLocation))
+                .Returns(new TemperatureRange(0, 10));
+            
             _fakeNowProvider = new Mock<INowProvider>();
-            _sut = new GetWeatherForecastQueryHandler(_temperatureRepositoryMock.Object, _fakeNowProvider.Object, _randomGenerator.Object);
+
+            _sut = new GetWeatherForecastQueryHandler(
+                _temperatureRepositoryMock.Object, 
+                _fakeNowProvider.Object, 
+                new FakeRandomGenerator(0));
         }
         
 
         [Theory]
         [InlineData(5)]
         [InlineData(14)]
-        public async Task WhenDays_And_LocationBonn(int days)
+        public async Task GenerateForecast_VerifyDates(int days)
         {
             // arrange
-            const string location = "germany/bonn";
-           _temperatureRepositoryMock
-                .Setup(x => x.Get(location))
-                .Returns(new TemperatureRange(0, 10));
-            
-            _randomGenerator.Setup(x => x.GetRange(It.IsAny<int>(), It.IsAny<int>())).Returns(8);
-
             var testTime = new DateTime(2021, 12, 01);
             _fakeNowProvider.Setup(x => x.Now()).Returns(testTime);
+            var request = new GetWeatherForecastQuery(days, AnyLocation);
 
             // act
-            var forecasts = await _sut.Handle(new GetWeatherForecastQuery(days, location), CancellationToken.None);
+            var forecasts = (await _sut.Handle(request, CancellationToken.None))
+                .ToList();
 
             // assert
-            for(int x = 0; x < days; x++)
+            for(var x = 0; x < days; x++)
             {
-                Assert.Equal(testTime.AddDays(x+1), forecasts.ToArray()[x].Date);
+                Assert.Equal(testTime.AddDays(x+1), forecasts[x].Date);
             }          
         }
     }
