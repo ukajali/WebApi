a23cd2104b99f71892020e92f3cc34f45df02bff
Bogdan Polak <bogdanpol@gmail.com>
2021-10-29 16:23:05 +0200

    Code review

--- WeatherForecast/Controllers/ClimateController.cs
+++ WeatherForecast/Controllers/ClimateController.cs
@@ -1,35 +1,24 @@
-﻿using AutoMapper;
+﻿using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using WeatherForecast.Dto;
 using WeatherForecast.WeatherForecastFeature;
 
 namespace WeatherForecast.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class ClimateController : ControllerBase
     {
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<ClimateController> _logger;
         private readonly IMediator _mediator;
-        private readonly IMapper _mapper;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger, IMediator mediator, IMapper mapper)
+        public ClimateController(
+            ILogger<ClimateController> logger, 
+            IMediator mediator)
         {
             _logger = logger;
             _mediator = mediator;
-            _mapper = mapper;
-        }
-
-        [HttpGet("{days}/{location}")]
-        public async Task<IActionResult> Get(int days, string location)
-        {
-            _logger.LogInformation("[GET] WeatherForecast. days:{days} location:{location}", days, location);
-            return Ok(_mapper.Map<List<WeatherForecastDto>>(await _mediator.Send(new GetWeatherForecastQuery(days, location))));
-
         }
 
         [HttpGet]
--- WeatherForecast/Repositories/DataBaseInMemory/MemoryDatabaseContext.cs
+++ WeatherForecast/Repositories/DataBaseInMemory/MemoryDatabaseContext.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using WeatherForecast.Contracts;
 using WeatherForecast.Model;
 
@@ -9,9 +7,11 @@ namespace WeatherForecast.Repositories.DataBaseInMemory
 {
     public class MemoryDatabaseContext : IDatabaseContext
     {
-        public IQueryable<Climate> LocationClimates => DatabaseInMemory.LocationClimates.AsQueryable();
+        private readonly IList<Climate> _locationClimates = new List<Climate>(DatabaseInMemory.LocationClimates);
 
-        public void AddClimate(Climate climate) => DatabaseInMemory.LocationClimates.Add(
+        public IQueryable<Climate> LocationClimates => _locationClimates.AsQueryable();
+
+        public void AddClimate(Climate climate) => _locationClimates.Add(
             new Climate
             {
                 Location = climate.Location,
@@ -21,14 +21,14 @@ namespace WeatherForecast.Repositories.DataBaseInMemory
 
         private static class DatabaseInMemory
         {
-            internal static readonly List<Climate> LocationClimates = new()
+            internal static readonly IReadOnlyList<Climate> LocationClimates = new List<Climate>
             {
-                new Climate { Location = "poland/krakow", LowTemperature = -15, HighTemperature = 38 },
-                new Climate { Location = "india/chennai", LowTemperature = -1, HighTemperature = 55 },
-                new Climate { Location = "usa/cleveland", LowTemperature = -10, HighTemperature = 42 },
-                new Climate { Location = "usa/new-york", LowTemperature = -10, HighTemperature = 42 },
-                new Climate { Location = "usa/san-francisco", LowTemperature = -1, HighTemperature = 49 },
-                new Climate { Location = "usa/redmond", LowTemperature = -12, HighTemperature = 38 }
+                new() { Location = "poland/krakow", LowTemperature = -15, HighTemperature = 38 },
+                new() { Location = "india/chennai", LowTemperature = -1, HighTemperature = 55 },
+                new() { Location = "usa/cleveland", LowTemperature = -10, HighTemperature = 42 },
+                new() { Location = "usa/new-york", LowTemperature = -10, HighTemperature = 42 },
+                new() { Location = "usa/san-francisco", LowTemperature = -1, HighTemperature = 49 },
+                new() { Location = "usa/redmond", LowTemperature = -12, HighTemperature = 38 }
             };
         }
     }
