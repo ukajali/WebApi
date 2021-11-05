d64141330e10b7d7fc15dba9d3361226ed4a3ba0
dplonka <dplonka@oeconnection.com>
2021-11-05 23:14:05 +0100

    Welcom Page Update

--- WeatherForecast/WebApi/WelcomePage.cs
+++ WeatherForecast/WebApi/WelcomePage.cs
@@ -1,5 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using WeatherForecast.Core.Contracts;
+using WeatherForecast.Core.Model;
 
 namespace WeatherForecast.WebApi
 {
@@ -7,22 +12,30 @@ namespace WeatherForecast.WebApi
     {
         public static async Task GetHtmlContent(HttpContext context)
         {
-            await context.Response.WriteAsync(WelcomePageHtml);
-        }
+            var services = context.RequestServices;
+            var dbContext = (IDatabaseContext)services.GetService(typeof(IDatabaseContext));
+            var locations = dbContext.LocationClimates.ToList();
 
-        public static async Task GetFavicon(HttpContext context)
+            await context.Response.WriteAsync(GenerateWelcomePageHtml(locations));
+        }
+        private static string GenerateHtmlLocationsList(List<Climate> locations)
         {
-            context.Response.ContentType = "image/svg+xml";
-            await context.Response.WriteAsync(SvgIcon); 
+            var locationsBilder = new StringBuilder();
+            locationsBilder.Append("<ul>");
+            foreach (var loc in locations)
+            {
+                locationsBilder.Append("<li>");
+                locationsBilder.Append(loc.Location);
+                locationsBilder.Append("</li>");
+            }
+            locationsBilder.Append("</ul>");
+            return locationsBilder.ToString();
+
         }
-
-        private const string SvgIcon =@"
-                <svg xmlns='http://www.w3.org/2000/svg' width='40' height='32' class='me-2' viewBox='0 0 118 94' role='img'>
-                    <title>Bootstrap icon</title>
-                    <path fill-rule='evenodd' clip-rule='evenodd' d='M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z' fill='lawngreen'></path>
-                </svg>";
-
-        private const string WelcomePageHtml = @"
+        private static string GenerateWelcomePageHtml(List<Climate> locationsList)
+        {
+            var locations = GenerateHtmlLocationsList(locationsList);
+            return $@"
                 <!DOCTYPE html>
                 <html lang='en'>
                 <head>
@@ -62,7 +75,7 @@ namespace WeatherForecast.WebApi
                             <div class='col-md-6'>
                                 <div class='h-100 p-5 bg-light border rounded-3'>
                                     <h2>Locations</h2>
-                                    <p>Supported locations: {locations}</p>
+                                    <p>Supported locations: {locations} </p>
                                 </div>
                             </div>
                         </div>
@@ -76,5 +89,20 @@ namespace WeatherForecast.WebApi
                 <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js' integrity='sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p' crossorigin='anonymous'></script>
                 </body>
                 </html>";
+        }
+       
+        public static async Task GetFavicon(HttpContext context)
+        {
+            context.Response.ContentType = "image/svg+xml";
+            await context.Response.WriteAsync(SvgIcon); 
+        }
+
+        private const string SvgIcon =@"
+                <svg xmlns='http://www.w3.org/2000/svg' width='40' height='32' class='me-2' viewBox='0 0 118 94' role='img'>
+                    <title>Bootstrap icon</title>
+                    <path fill-rule='evenodd' clip-rule='evenodd' d='M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z' fill='lawngreen'></path>
+                </svg>";
+
+     
     }
 }
